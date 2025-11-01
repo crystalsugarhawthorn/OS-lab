bfbf0cc7b773e0fd4951896bb8dc8bb73f47b839
Hao <1572219862@qq.com>
2025-11-01 19:54:09 +0800

    lab3-sync.h-comment

--- lab3/kern/sync/sync.h
+++ lab3/kern/sync/sync.h
@@ -19,10 +19,16 @@ static inline void __intr_restore(bool flag) {
     }
 }
 
+// 原本宏展开后需要是一个赋值语句
+// do while(0) 保证宏展开后在任何语法环境下都表现得像一个单独的语句，以及避免“悬挂else”问题。
+
 #define local_intr_save(x) \
     do {                   \
         x = __intr_save(); \
     } while (0)
+
+// 下面这个宏本身就是一个函数调用语句，已经是单一语句了，所以不加 do-while(0) 也不会有语法问题
+
 #define local_intr_restore(x) __intr_restore(x);
 
 #endif /* !__KERN_SYNC_SYNC_H__ */
