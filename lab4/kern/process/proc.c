f847887a4c6f201d919aa85ed96250a78dc5ef8b
Hao <1572219862@qq.com>
2025-11-13 16:02:22 +0800

    lab4-add-id

--- lab4/kern/process/proc.c
+++ lab4/kern/process/proc.c
@@ -187,7 +187,7 @@ void proc_run(struct proc_struct *proc)
 {
     if (proc != current)
     {
-        // LAB4:EXERCISE3 YOUR CODE
+        // LAB4:EXERCISE3 YOUR CODE 2313892 2313851
         /*
          * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
          * MACROs or Functions:
